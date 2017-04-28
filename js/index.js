e75aa60ec7084018c825aa79d9ba57eed5a9b315
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-04-28 20:01:56 +0300

    Set up tooling; implement keygen function

---
+++ js/index.js
@@ -0,0 +1,63 @@
+import $ from 'balajs';
+
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const special = '`~!@#$%^&*()-=_+[]{}|;\':",./<>?';
+const hex = '123456789ABCDEF';
+
+function keyGen(
+    length,
+    useLowerCase = true,
+    useUpperCase = true,
+    useNumbers = true,
+    useSpecial = true,
+    useHex = false
+) {
+    let chars = '';
+    let key = '';
+
+    if (useLowerCase) chars += lowerCase;
+    if (useUpperCase) chars += upperCase;
+    if (useNumbers) chars += numbers;
+    if (useSpecial) chars += special;
+    if (useHex) chars += hex;
+
+    for (let i = 0; i < length; i++) {
+        key += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return key;
+}
+
+
+function getKey(strength) {
+    switch (strength) {
+    case 'decent_pw':
+        return keyGen(10, true, true, true, false, false);
+    case 'strong_pw':
+        return keyGen(15, true, true, true, true, false);
+    case 'ft_knox_pw':
+        return keyGen(30, true, true, true, true, false);
+    case 'ci_key':
+        return keyGen(32, true, true, true, false, false);
+    case '160_wpa':
+        return keyGen(20, true, true, true, true, false);
+    case '504_wpa':
+        return keyGen(63, true, true, true, true, false);
+    case '64_wep':
+        return keyGen(5, false, false, false, false, true);
+    case '128_wep':
+        return keyGen(13, false, false, false, false, true);
+    case '152_wep':
+        return keyGen(16, false, false, false, false, true);
+    case '256_wep':
+        return keyGen(29, false, false, false, false, true);
+    default:
+        throw Error(`No such strength "${strength}"`);
+    }
+}
+
+for (const node of $('input[data-strength]')) {
+    node.value = getKey(node.dataset.strength);
+}
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SplitByPathPlugin = require('webpack-split-by-path');
+const path = require('path');
+const { argv } = require('optimist');
+
+const { NODE_ENV } = process.env;
+const entry = {
+    app: []
+};
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: 'index.html',
+        chunksSortMode: (a, b) => {
+            const order = ['manifest', 'vendor', 'app'];
+            const nameA = a.names[0];
+            const nameB = b.names[0];
+
+            return order.indexOf(nameA) - order.indexOf(nameB);
+        }
+    }),
+    new SplitByPathPlugin([{
+        name: 'vendor',
+        path: path.join(__dirname, 'node_modules/'),
+    }]),
+    function saveChunksPlugin() {
+        this.plugin('emit', (compilation, callback) => {
+            const chunks = [];
+            for (const { files: [jsName] } of compilation.chunks) {
+                chunks.push(jsName);
+            }
+            const chunksJSON = JSON.stringify(chunks);
+            // eslint-disable-next-line no-param-reassign
+            compilation.assets['chunks.json'] = {
+                source: () => chunksJSON,
+                size: () => chunksJSON.length
+            };
+            callback();
+        });
+    }
+];
+
+if (NODE_ENV === 'development') {
+    const { port } = argv;
+
+    entry.app.push(`webpack-dev-server/client?http://localhost:${port}`);
+    plugins.push(new OpenBrowserPlugin({
+        url: `http://localhost:${port}`,
+        ignoreErrors: true
+    }));
+}
+
+let filename;
+let chunkFilename;
+
+if (NODE_ENV === 'development') {
+    filename = 'js/[name].js';
+    chunkFilename = 'js/[id].[name].chunk.js';
+} else {
+    filename = 'js/[name]-[chunkhash].js';
+    chunkFilename = 'js/[id]-[chunkhash].[name].chunk.js';
+}
+
+entry.app.push(
+    'babel-polyfill',
+    './js/index'
+);
+
+plugins.push(new CopyWebpackPlugin([
+    { from: 'static', to: '.' },
+]));
+
+module.exports = {
+    entry,
+    plugins,
+    context: __dirname,
+    output: {
+        filename,
+        chunkFilename,
+        path: path.resolve('dist/'),
+        library: 'app',
+        libraryTarget: 'var'
+    },
+    module: {
+        rules: [{
+            test: /.js?$/,
+            use: ['babel-loader'],
+            include: path.resolve('js/')
+        }]
+    },
+    devtool: 'source-map'
+};
